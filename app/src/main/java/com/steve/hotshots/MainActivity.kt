f73c10814dee6b861ba1780adcb3925d69fae51b
Steve Bao <34757708+stevebao2000@users.noreply.github.com>
2021-06-29 21:09:46 -0700

    Add permission check

--- app/src/main/java/com/steve/hotshots/MainActivity.kt
+++ app/src/main/java/com/steve/hotshots/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.steve.hotshots
 
+import android.content.pm.PackageManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.steve.hotshots.databinding.ActivityMainBinding
 import com.steve.hotshots.ui.EntryFragment
@@ -10,11 +14,16 @@ import com.steve.hotshots.ui.ListFragment
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
+    val INTERNET_PERMISSION = 101
+    val NETWORK_STATE = 102
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        checkForPermissions(android.Manifest.permission.INTERNET, "Internet", INTERNET_PERMISSION)
+        checkForPermissions(android.Manifest.permission.ACCESS_NETWORK_STATE, "Network State", NETWORK_STATE)
         // when activity create
         showListFragment()
     }
@@ -29,5 +38,22 @@ class MainActivity : AppCompatActivity() {
         supportFragmentManager.beginTransaction()
             .replace(R.id.container, EntryFragment.newInstance(index))
             .commitNow()
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+    }
+    private fun checkForPermissions(permission: String, name: String, requestCode: Int) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            when {
+                ContextCompat.checkSelfPermission(applicationContext, permission) == PackageManager.PERMISSION_DENIED ->
+                    ActivityCompat.requestPermissions(this, arrayOf(permission), requestCode)
+            }
+        }
     }
 }
